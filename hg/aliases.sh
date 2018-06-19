db7269363290c59de0737402953f0208b4570e71
Marco Leogrande <dark.knight.ita@gmail.com>
2018-06-18 20:59:16 -0700

    Add skeleton for Mercurial integration

---
+++ hg/aliases.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#    Bash aliases for Mercurial directories
+#    Copyright (C) 2018  Marco Leogrande
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Import the binutils/ subdirectory as part of the PATH.
+SCRIPTDIR=$(readlink -e "$(dirname "${BASH_SOURCE[0]}")" )
+if [[ -n "${SCRIPTDIR}" ]]; then
+  export PATH="${PATH}:${SCRIPTDIR}/binutils/"
+else
+  echo "${BASH_SOURCE[0]} did not point to anything useful"
+fi
+unset SCRIPTDIR
